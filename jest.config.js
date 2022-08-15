2d57ba12f92751f7feb442e9eeb61852d469c7ec
Lino Fischer <linofischer@outlook.com>
2022-08-15 10:33:37 +0200

    Add jest coverageThreshold

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,13 @@ module.exports = {
     testEnvironment: "jsdom",
     moduleNameMapper: {
         ".(css|less|scss)$": "identity-obj-proxy",
-      },
-  };
+    },
+    coverageThreshold: {
+        global: {
+            branches: 100,
+            functions: 100,
+            lines: 100,
+            statements: 100
+        }
+    }
+};
